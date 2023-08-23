afc226f9ce6d6a293acf19017c71da18067d9975
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2023-08-23 17:58:43 -0400

    installed tailwind css and corrected configuration for intellisense plugin

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import Header from './components/Header/Header'
+
+function App() {
+
+
+  return (
+    <>
+     <Header/>
+     <p className='text-center'>center</p>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <div>
+      <p className="text-2xs text-center font-bold underline">
+        Password Generator
+      </p>
+    </div>
+  );
+};
+
+export default Header;
